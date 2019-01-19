8bdc1fe742d220a0385be4b714244c84c236ef05
bokwoon <bokwoon.c@gmail.com>
2019-01-19 20:22:46 +0800

    got the repl window dimensions down
    for now

--- termtutor.go
+++ termtutor.go
@@ -26,6 +26,29 @@ func main() {
 }
 
 func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("repl_window", 0, 0, maxX-1, maxY-10); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "repl_window"
+		v.Editable = true
+		v.Wrap = true
+		if _, err = setCurrentViewOnTop(g, "repl_window"); err != nil {
+			return err
+		}
+	}
+	if v, err := g.SetView("repl_textbox", 0, maxY-9, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "repl_textbox"
+		v.Editable = true
+		v.Wrap = true
+		if _, err = setCurrentViewOnTop(g, "repl_textbox"); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -45,3 +68,12 @@ func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
 }
 // Handler functions
+
+// Helper functions
+func setCurrentViewOnTop(g *gocui.Gui, name string) (*gocui.View, error) {
+	if _, err := g.SetCurrentView(name); err != nil {
+		return nil, err
+	}
+	return g.SetViewOnTop(name)
+}
+// Helper functions
