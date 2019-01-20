1b3828e1e35d4feeaa2aee53b5a6f9f7f74aea5a
bokwoon <bokwoon.c@gmail.com>
2019-01-20 09:24:34 +0800

    'finished' product
    it's horribly written code that goes against my best wishes but I had to
    deliver

--- termtutor.go
+++ termtutor.go
@@ -1,22 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/jroimartin/gocui"
 	"github.com/robertkrimen/otto"
 )
 
+type QnA struct {
+	Question string
+	Answer   string
+}
+
 var (
-	vm = otto.New()
-	rarr = []rune("")
+	vm    = otto.New()
+	rarr  = []rune("")
 	rarrp = 0
-	mode = 1
+	mode  = 1
 	replm = 1
+	qnas  []QnA
+	qn = 0
 )
 
 func main() {
+	// Read in json data
+	file, err := ioutil.ReadFile("./data.json")
+	if err != nil {
+		fmt.Printf("File error: %v\n", err)
+		os.Exit(1)
+	}
+	err = json.Unmarshal(file, &qnas)
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		log.Panicln(err)
@@ -38,15 +59,15 @@ func main() {
 
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("repl_window", 0, 0, maxX-1, maxY-10); err != nil {
+	if v, err := g.SetView("javascript_repl", 0, 0, maxX-1, maxY-10); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		v.Title = "repl_window"
+		v.Title = "javascript_repl"
 		v.Editable = false
 		v.Wrap = true
 		v.Autoscroll = true
-		if _, err = setCurrentViewOnTop(g, "repl_window"); err != nil {
+		if _, err = setCurrentViewOnTop(g, "javascript_repl"); err != nil {
 			return err
 		}
 	}
@@ -68,6 +89,10 @@ func layout(g *gocui.Gui) error {
 		v.Title = "questions"
 		v.Editable = true
 		v.Wrap = true
+		v.Autoscroll = true
+		qn = 0
+		fmt.Fprintln(v, qnas[qn].Question + "\n\n\n")
+		qn += 1
 		if _, err = setCurrentViewOnTop(g, "questions"); err != nil {
 			return err
 		}
@@ -79,6 +104,7 @@ func layout(g *gocui.Gui) error {
 		v.Title = "answers"
 		v.Editable = true
 		v.Wrap = true
+		v.Autoscroll = true
 		if _, err = setCurrentViewOnTop(g, "answers"); err != nil {
 			return err
 		}
@@ -93,20 +119,20 @@ func quit(g *gocui.Gui, v *gocui.View) error {
 
 // repl_textbox
 func eval_repltextbox(g *gocui.Gui, v *gocui.View) error {
-	repl_window, _ := g.View("repl_window")
+	javascript_repl, _ := g.View("javascript_repl")
 	cmd := string(rarr)
 	rarr = []rune{}
 	v.SetCursor(0, 0)
 	v.Clear()
 	if cmd != "" {
-		fmt.Fprintln(repl_window, "> "+cmd)
+		fmt.Fprintln(javascript_repl, "> "+cmd)
 		v.SetOrigin(0, 0)
 		v.SetCursor(0, 0)
 		v.Clear()
 		value, err := vm.Eval(cmd)
-		fmt.Fprintln(repl_window, value)
+		fmt.Fprintln(javascript_repl, value)
 		if err != nil {
-			fmt.Fprintln(repl_window, err)
+			fmt.Fprintln(javascript_repl, err)
 		}
 	}
 	return nil
@@ -132,6 +158,7 @@ func setCurrentViewOnTop(g *gocui.Gui, name string) (*gocui.View, error) {
 	}
 	return g.SetViewOnTop(name)
 }
+
 // End Helper functions
 
 func initKeybindings(g *gocui.Gui) error {
@@ -156,9 +183,14 @@ func initKeybindings(g *gocui.Gui) error {
 	// if err := g.SetKeybinding("repl_textbox", gocui.KeyTab, gocui.ModNone, togglereplm); err != nil {
 	// 	log.Panicln(err)
 	// }
-	// if err := g.SetKeybinding("repl_window", gocui.KeyTab, gocui.ModNone, togglereplm); err != nil {
+	// if err := g.SetKeybinding("javascript_repl", gocui.KeyTab, gocui.ModNone, togglereplm); err != nil {
 	// 	log.Panicln(err)
 	// }
+
+	// answers
+	if err := g.SetKeybinding("answers", gocui.KeyEnter, gocui.ModNone, nextquestion); err != nil {
+		log.Panicln(err)
+	}
 
 	// Swap modes
 	if err := g.SetKeybinding("repl_textbox", gocui.KeyCtrlQ, gocui.ModNone, switchlesson); err != nil {
@@ -225,7 +257,7 @@ func arrowright(g *gocui.Gui, v *gocui.View) error {
 }
 func togglereplm(g *gocui.Gui, v *gocui.View) error {
 	if replm == 1 {
-		g.SetCurrentView("repl_window")
+		g.SetCurrentView("javascript_repl")
 		replm = 0
 	} else {
 		g.SetCurrentView("repl_textbox")
@@ -240,8 +272,33 @@ func switchlesson(g *gocui.Gui, v *gocui.View) error {
 	return nil
 }
 func switchrepl(g *gocui.Gui, v *gocui.View) error {
-	g.SetViewOnTop("repl_window")
+	g.SetViewOnTop("javascript_repl")
 	g.SetViewOnTop("repl_textbox")
 	g.SetCurrentView("repl_textbox")
+	return nil
+}
+func nextquestion(g *gocui.Gui, v *gocui.View) error {
+	w, _ := g.View("questions")
+	wlen := len(w.BufferLines())
+	if qn > 0 {
+		fmt.Fprintln(v, "\n" + qnas[qn-1].Answer)
+	} else {
+		fmt.Fprintln(v, "\n" + qnas[9].Answer)
+	}
+	fmt.Fprintln(w, qnas[qn].Question + "\n\n\n")
+	qn += 1
+	qn = qn % 10
+	newlines(v, w, wlen)
+	return nil
+}
+func newlines(v *gocui.View, w *gocui.View, wlen int) error {
+	// wlen := len(w.BufferLines())
+	vlen := len(v.BufferLines())
+	n := wlen - vlen
+	for i := 0; i < n - 1; i++ {
+		fmt.Fprintf(v, "\n")
+	}
+	vlen = len(v.BufferLines())
+	v.SetCursor(0, vlen)
 	return nil
 }
